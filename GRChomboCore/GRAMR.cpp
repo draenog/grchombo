37512a700833ba27516995a4bf76cc4eff662d11
Kacper Korne <kk562@damtp.cam.ac.uk>
2017-05-23 13:25:48 -0500

    Don't duplicate code from AMR::conclude()

--- GRChomboCore/GRAMR.cpp
+++ GRChomboCore/GRAMR.cpp
@@ -49,63 +49,10 @@ void GRAMR::setDefaultValues()
 
 }
 
-void AMR::conclude() const
+void GRAMR::conclude() const
 {
-  CH_TIME("AMR::conclude");
 
-  CH_assert(isDefined());
-  CH_assert(isSetUp());
-
-  if (m_verbosity >= 3)
-    {
-      pout() << "AMR::conclude" << endl;
-    }
-
-  if ((m_plot_interval >= 0) || (m_plot_period >= 0.0))
-    {
-      writePlotFile();
-    }
-
-  if ((m_checkpoint_interval >= 0)     &&
-      (m_lastcheck_step != m_cur_step) &&
-      (m_restart_step != m_cur_step))
-    {
-      writeCheckpointFile();
-    }
-
-  // Call any scheduled functions. This is placed after plotting and
-  // checkpointing so that the plotting functions can congeal plot files.
-  if (!m_scheduler.isNull())
-    {
-      m_scheduler->conclude(m_cur_step, m_cur_time);
-    }
-
-  // Call the hooks for the AMRLevel objects.                                      
-  for (int level = 0; level <= m_finest_level; ++level)
-    m_amrlevels[level]->conclude(m_cur_step);
-
-  if (m_verbosity >= 2)
-    {
-      long long total_cell_updates = 0;
-      for (int ll = 0; ll < m_max_level+1; ll++)
-        {
-          total_cell_updates += m_cell_updates[ll];
-
-#ifdef CH_OSF1
-          pout() << "number of points updated at level " << ll <<" = " <<
-	    (Real)m_cell_updates[ll] << endl;
-#else
-          pout() << "number of points updated at level " << ll <<" = " <<
-	    m_cell_updates[ll] << endl;
-#endif
-        }
-#ifdef CH_OSF1
-      pout() << "total number of points updated = " << (Real)total_cell_updates << endl;
-#else
-      pout() << "total number of points updated = " << total_cell_updates << endl;
-#endif
-    }
-
+  AMR::conclude();
 #ifdef USE_CATALYST
   m_insitu.finalise();
 #endif

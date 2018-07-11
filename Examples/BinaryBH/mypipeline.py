4f5fbc8574bec8e7d19b73358c8022049bf40458
Kacper Kornet <kk562@damtp.cam.ac.uk>
2018-07-11 16:25:09 +0100

    New paramerate file (slightly cheaper) and pipeline

---
+++ Examples/BinaryBH/mypipeline.py
@@ -0,0 +1,219 @@
+
+from paraview.simple import *
+from paraview import coprocessing
+
+
+#--------------------------------------------------------------
+# Code generated from cpstate.py to create the CoProcessor.
+# paraview version 5.5.2
+
+#--------------------------------------------------------------
+# Global screenshot output options
+imageFileNamePadding=0
+rescale_lookuptable=False
+
+
+# ----------------------- CoProcessor definition -----------------------
+
+def CreateCoProcessor():
+  def _CreatePipeline(coprocessor, datadescription):
+    class Pipeline:
+      # state file generated using paraview version 5.5.2
+
+      # ----------------------------------------------------------------
+      # setup views used in the visualization
+      # ----------------------------------------------------------------
+
+      # trace generated using paraview version 5.5.2
+
+      #### disable automatic camera reset on 'Show'
+      paraview.simple._DisableFirstRenderCameraReset()
+
+      # get the material library
+      materialLibrary1 = GetMaterialLibrary()
+
+      # Create a new 'Render View'
+      renderView1 = CreateView('RenderView')
+      renderView1.ViewSize = [1107, 645]
+      renderView1.AxesGrid = 'GridAxes3DActor'
+      renderView1.CenterOfRotation = [256.0, 256.0, 256.0]
+      renderView1.StereoType = 0
+      renderView1.CameraPosition = [188.02054550291314, -849.130273625168, 1563.314060500172]
+      renderView1.CameraFocalPoint = [256.0, 256.0, 256.0]
+      renderView1.CameraViewUp = [-0.010802768101508832, 0.7639234377257922, 0.6452164609606243]
+      renderView1.CameraParallelScale = 443.40500673763256
+      renderView1.Background = [0.32, 0.34, 0.43]
+      renderView1.OSPRayMaterialLibrary = materialLibrary1
+
+      # init the 'GridAxes3DActor' selected for 'AxesGrid'
+      renderView1.AxesGrid.XTitleFontFile = ''
+      renderView1.AxesGrid.YTitleFontFile = ''
+      renderView1.AxesGrid.ZTitleFontFile = ''
+      renderView1.AxesGrid.XLabelFontFile = ''
+      renderView1.AxesGrid.YLabelFontFile = ''
+      renderView1.AxesGrid.ZLabelFontFile = ''
+
+      # register the view with coprocessor
+      # and provide it with information such as the filename to use,
+      # how frequently to write the images, etc.
+      coprocessor.RegisterView(renderView1,
+          filename='image_%t.png', freq=1, fittoscreen=0, magnification=1, width=1107, height=645, cinema={})
+      renderView1.ViewTime = datadescription.GetTime()
+
+      # ----------------------------------------------------------------
+      # restore active view
+      SetActiveView(renderView1)
+      # ----------------------------------------------------------------
+
+      # ----------------------------------------------------------------
+      # setup the data processing pipelines
+      # ----------------------------------------------------------------
+
+      # create a new 'VisItChomboReader'
+      # create a producer from a simulation input
+      plot_wo_catalyst_0000003dhdf5 = coprocessor.CreateProducer(datadescription, 'input')
+
+      # create a new 'Slice'
+      slice1 = Slice(Input=plot_wo_catalyst_0000003dhdf5)
+      slice1.SliceType = 'Plane'
+      slice1.SliceOffsetValues = [0.0]
+
+      # init the 'Plane' selected for 'SliceType'
+      slice1.SliceType.Origin = [256.0, 256.0, 256.0]
+      slice1.SliceType.Normal = [0.0, 0.0, 1.0]
+
+      # ----------------------------------------------------------------
+      # setup the visualization in view 'renderView1'
+      # ----------------------------------------------------------------
+
+      # show data from slice1
+      slice1Display = Show(slice1, renderView1)
+
+      # get color transfer function/color map for 'Psi4r'
+      psi4rLUT = GetColorTransferFunction('Psi4r')
+      psi4rLUT.RGBPoints = [-0.00215938, 0.231373, 0.298039, 0.752941, -0.0005796899999999999, 0.865003, 0.865003, 0.865003, 0.001, 0.705882, 0.0156863, 0.14902]
+      psi4rLUT.ScalarRangeInitialized = 1.0
+
+      # trace defaults for the display properties.
+      slice1Display.Representation = 'Surface'
+      slice1Display.ColorArrayName = ['CELLS', 'Psi4r']
+      slice1Display.LookupTable = psi4rLUT
+      slice1Display.OSPRayScaleFunction = 'PiecewiseFunction'
+      slice1Display.SelectOrientationVectors = 'None'
+      slice1Display.ScaleFactor = 51.2
+      slice1Display.SelectScaleArray = 'None'
+      slice1Display.GlyphType = 'Arrow'
+      slice1Display.GlyphTableIndexArray = 'None'
+      slice1Display.GaussianRadius = 2.56
+      slice1Display.SetScaleArray = [None, '']
+      slice1Display.ScaleTransferFunction = 'PiecewiseFunction'
+      slice1Display.OpacityArray = [None, '']
+      slice1Display.OpacityTransferFunction = 'PiecewiseFunction'
+      slice1Display.DataAxesGrid = 'GridAxesRepresentation'
+      slice1Display.SelectionCellLabelFontFile = ''
+      slice1Display.SelectionPointLabelFontFile = ''
+      slice1Display.PolarAxes = 'PolarAxesRepresentation'
+
+      # init the 'GridAxesRepresentation' selected for 'DataAxesGrid'
+      slice1Display.DataAxesGrid.XTitleFontFile = ''
+      slice1Display.DataAxesGrid.YTitleFontFile = ''
+      slice1Display.DataAxesGrid.ZTitleFontFile = ''
+      slice1Display.DataAxesGrid.XLabelFontFile = ''
+      slice1Display.DataAxesGrid.YLabelFontFile = ''
+      slice1Display.DataAxesGrid.ZLabelFontFile = ''
+
+      # init the 'PolarAxesRepresentation' selected for 'PolarAxes'
+      slice1Display.PolarAxes.PolarAxisTitleFontFile = ''
+      slice1Display.PolarAxes.PolarAxisLabelFontFile = ''
+      slice1Display.PolarAxes.LastRadialAxisTextFontFile = ''
+      slice1Display.PolarAxes.SecondaryRadialAxesTextFontFile = ''
+
+      # setup the color legend parameters for each legend in this view
+
+      # get color legend/bar for psi4rLUT in view renderView1
+      psi4rLUTColorBar = GetScalarBar(psi4rLUT, renderView1)
+      psi4rLUTColorBar.Title = 'Psi4r'
+      psi4rLUTColorBar.ComponentTitle = ''
+      psi4rLUTColorBar.TitleFontFile = ''
+      psi4rLUTColorBar.LabelFontFile = ''
+
+      # set color bar visibility
+      psi4rLUTColorBar.Visibility = 1
+
+      # show color legend
+      slice1Display.SetScalarBarVisibility(renderView1, True)
+
+      # ----------------------------------------------------------------
+      # setup color maps and opacity mapes used in the visualization
+      # note: the Get..() functions create a new object, if needed
+      # ----------------------------------------------------------------
+
+      # get opacity transfer function/opacity map for 'Psi4r'
+      psi4rPWF = GetOpacityTransferFunction('Psi4r')
+      psi4rPWF.Points = [-0.00215938, 0.0, 0.5, 0.0, 0.001, 1.0, 0.5, 0.0]
+      psi4rPWF.ScalarRangeInitialized = 1
+
+      # ----------------------------------------------------------------
+      # finally, restore active source
+      SetActiveSource(slice1)
+      # ----------------------------------------------------------------
+    return Pipeline()
+
+  class CoProcessor(coprocessing.CoProcessor):
+    def CreatePipeline(self, datadescription):
+      self.Pipeline = _CreatePipeline(self, datadescription)
+
+  coprocessor = CoProcessor()
+  # these are the frequencies at which the coprocessor updates.
+  freqs = {'MyBH': [1, 1]}
+  coprocessor.SetUpdateFrequencies(freqs)
+  return coprocessor
+
+
+#--------------------------------------------------------------
+# Global variable that will hold the pipeline for each timestep
+# Creating the CoProcessor object, doesn't actually create the ParaView pipeline.
+# It will be automatically setup when coprocessor.UpdateProducers() is called the
+# first time.
+coprocessor = CreateCoProcessor()
+
+#--------------------------------------------------------------
+# Enable Live-Visualizaton with ParaView and the update frequency
+coprocessor.EnableLiveVisualization(False, 1)
+
+# ---------------------- Data Selection method ----------------------
+
+def RequestDataDescription(datadescription):
+    "Callback to populate the request for current timestep"
+    global coprocessor
+    if datadescription.GetForceOutput() == True:
+        # We are just going to request all fields and meshes from the simulation
+        # code/adaptor.
+        for i in range(datadescription.GetNumberOfInputDescriptions()):
+            datadescription.GetInputDescription(i).AllFieldsOn()
+            datadescription.GetInputDescription(i).GenerateMeshOn()
+        return
+
+    # setup requests for all inputs based on the requirements of the
+    # pipeline.
+    coprocessor.LoadRequestedData(datadescription)
+
+# ------------------------ Processing method ------------------------
+
+def DoCoProcessing(datadescription):
+    "Callback to do co-processing for current timestep"
+    global coprocessor
+
+    # Update the coprocessor by providing it the newly generated simulation data.
+    # If the pipeline hasn't been setup yet, this will setup the pipeline.
+    coprocessor.UpdateProducers(datadescription)
+
+    # Write output data, if appropriate.
+    coprocessor.WriteData(datadescription);
+
+    # Write image capture (Last arg: rescale lookup table), if appropriate.
+    coprocessor.WriteImages(datadescription, rescale_lookuptable=rescale_lookuptable,
+        image_quality=0, padding_amount=imageFileNamePadding)
+
+    # Live Visualization, if enabled.
+    coprocessor.DoLiveVisualization(datadescription, "localhost", 22222)

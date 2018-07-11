cbb258dd02190d0532ea9bbd98ec95ea9d0cc7f3
Kacper Kornet <kk562@damtp.cam.ac.uk>
2018-07-11 21:43:33 +0000

    Version working on theta

--- Examples/BinaryBH/newpipeline.py
+++ Examples/BinaryBH/newpipeline.py
@@ -34,14 +34,14 @@ def CreateCoProcessor():
 
       # Create a new 'Render View'
       renderView1 = CreateView('RenderView')
-      renderView1.ViewSize = [1107, 645]
+      renderView1.ViewSize = [844, 547]
+      renderView1.InteractionMode = '2D'
       renderView1.AxesGrid = 'GridAxes3DActor'
-      renderView1.CenterOfRotation = [256.0, 256.0, 256.0]
+      renderView1.CenterOfRotation = [256.00000381469727, 256.00000381469727, 256.00000762939453]
       renderView1.StereoType = 0
-      renderView1.CameraPosition = [188.02054550291314, -849.130273625168, 1563.314060500172]
-      renderView1.CameraFocalPoint = [256.0, 256.0, 256.0]
-      renderView1.CameraViewUp = [-0.010802768101508832, 0.7639234377257922, 0.6452164609606243]
-      renderView1.CameraParallelScale = 443.40500673763256
+      renderView1.CameraPosition = [256.0, 256.0, 10256.0]
+      renderView1.CameraFocalPoint = [256.00000381469727, 256.00000381469727, 256.00000762939453]
+      renderView1.CameraParallelScale = 546.9664114379779
       renderView1.Background = [0.32, 0.34, 0.43]
       renderView1.OSPRayMaterialLibrary = materialLibrary1
 
@@ -57,7 +57,7 @@ def CreateCoProcessor():
       # and provide it with information such as the filename to use,
       # how frequently to write the images, etc.
       coprocessor.RegisterView(renderView1,
-          filename='image_%t.png', freq=1, fittoscreen=0, magnification=1, width=1107, height=645, cinema={})
+          filename='image_%t.png', freq=1, fittoscreen=0, magnification=1, width=844, height=547, cinema={})
       renderView1.ViewTime = datadescription.GetTime()
 
       # ----------------------------------------------------------------
@@ -86,55 +86,18 @@ def CreateCoProcessor():
       # setup the visualization in view 'renderView1'
       # ----------------------------------------------------------------
 
-      # show data from plot_wo_catalyst_0000003dhdf5
-      plot_wo_catalyst_0000003dhdf5Display = Show(plot_wo_catalyst_0000003dhdf5, renderView1)
-
-      # trace defaults for the display properties.
-      plot_wo_catalyst_0000003dhdf5Display.Representation = 'Outline'
-      plot_wo_catalyst_0000003dhdf5Display.ColorArrayName = [None, '']
-      plot_wo_catalyst_0000003dhdf5Display.OSPRayScaleFunction = 'PiecewiseFunction'
-      plot_wo_catalyst_0000003dhdf5Display.SelectOrientationVectors = 'None'
-      plot_wo_catalyst_0000003dhdf5Display.ScaleFactor = 51.2
-      plot_wo_catalyst_0000003dhdf5Display.SelectScaleArray = 'None'
-      plot_wo_catalyst_0000003dhdf5Display.GlyphType = 'Arrow'
-      plot_wo_catalyst_0000003dhdf5Display.GlyphTableIndexArray = 'None'
-      plot_wo_catalyst_0000003dhdf5Display.GaussianRadius = 2.56
-      plot_wo_catalyst_0000003dhdf5Display.SetScaleArray = [None, '']
-      plot_wo_catalyst_0000003dhdf5Display.ScaleTransferFunction = 'PiecewiseFunction'
-      plot_wo_catalyst_0000003dhdf5Display.OpacityArray = [None, '']
-      plot_wo_catalyst_0000003dhdf5Display.OpacityTransferFunction = 'PiecewiseFunction'
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid = 'GridAxesRepresentation'
-      plot_wo_catalyst_0000003dhdf5Display.SelectionCellLabelFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.SelectionPointLabelFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.PolarAxes = 'PolarAxesRepresentation'
-      plot_wo_catalyst_0000003dhdf5Display.ScalarOpacityUnitDistance = 6.913326157867665
-
-      # init the 'GridAxesRepresentation' selected for 'DataAxesGrid'
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid.XTitleFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid.YTitleFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid.ZTitleFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid.XLabelFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid.YLabelFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.DataAxesGrid.ZLabelFontFile = ''
-
-      # init the 'PolarAxesRepresentation' selected for 'PolarAxes'
-      plot_wo_catalyst_0000003dhdf5Display.PolarAxes.PolarAxisTitleFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.PolarAxes.PolarAxisLabelFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.PolarAxes.LastRadialAxisTextFontFile = ''
-      plot_wo_catalyst_0000003dhdf5Display.PolarAxes.SecondaryRadialAxesTextFontFile = ''
-
       # show data from slice1
       slice1Display = Show(slice1, renderView1)
 
-      # get color transfer function/color map for 'chi'
-      chiLUT = GetColorTransferFunction('chi')
-      chiLUT.RGBPoints = [0.0009853932076415149, 0.231373, 0.298039, 0.752941, 0.49542285684288034, 0.865003, 0.865003, 0.865003, 0.9898603204781191, 0.705882, 0.0156863, 0.14902]
-      chiLUT.ScalarRangeInitialized = 1.0
+      # get color transfer function/color map for 'Psi4r'
+      psi4rLUT = GetColorTransferFunction('Psi4r')
+      psi4rLUT.RGBPoints = [-4.09441e-07, 0.231373, 0.298039, 0.752941, -1.33798e-07, 0.865003, 0.865003, 0.865003, 1.4184500000000004e-07, 0.705882, 0.0156863, 0.14902]
+      psi4rLUT.ScalarRangeInitialized = 1.0
 
       # trace defaults for the display properties.
       slice1Display.Representation = 'Surface'
-      slice1Display.ColorArrayName = ['CELLS', 'chi']
-      slice1Display.LookupTable = chiLUT
+      slice1Display.ColorArrayName = ['CELLS', 'Psi4r']
+      slice1Display.LookupTable = psi4rLUT
       slice1Display.OSPRayScaleFunction = 'PiecewiseFunction'
       slice1Display.SelectOrientationVectors = 'None'
       slice1Display.ScaleFactor = 51.2
@@ -167,15 +130,15 @@ def CreateCoProcessor():
 
       # setup the color legend parameters for each legend in this view
 
-      # get color legend/bar for chiLUT in view renderView1
-      chiLUTColorBar = GetScalarBar(chiLUT, renderView1)
-      chiLUTColorBar.Title = 'chi'
-      chiLUTColorBar.ComponentTitle = ''
-      chiLUTColorBar.TitleFontFile = ''
-      chiLUTColorBar.LabelFontFile = ''
+      # get color legend/bar for psi4rLUT in view renderView1
+      psi4rLUTColorBar = GetScalarBar(psi4rLUT, renderView1)
+      psi4rLUTColorBar.Title = 'Psi4r'
+      psi4rLUTColorBar.ComponentTitle = ''
+      psi4rLUTColorBar.TitleFontFile = ''
+      psi4rLUTColorBar.LabelFontFile = ''
 
       # set color bar visibility
-      chiLUTColorBar.Visibility = 1
+      psi4rLUTColorBar.Visibility = 1
 
       # show color legend
       slice1Display.SetScalarBarVisibility(renderView1, True)
@@ -185,10 +148,10 @@ def CreateCoProcessor():
       # note: the Get..() functions create a new object, if needed
       # ----------------------------------------------------------------
 
-      # get opacity transfer function/opacity map for 'chi'
-      chiPWF = GetOpacityTransferFunction('chi')
-      chiPWF.Points = [0.0009853932076415149, 0.0, 0.5, 0.0, 0.9898603204781191, 1.0, 0.5, 0.0]
-      chiPWF.ScalarRangeInitialized = 1
+      # get opacity transfer function/opacity map for 'Psi4r'
+      psi4rPWF = GetOpacityTransferFunction('Psi4r')
+      psi4rPWF.Points = [-4.09441e-07, 0.0, 0.5, 0.0, 1.4184500000000004e-07, 1.0, 0.5, 0.0]
+      psi4rPWF.ScalarRangeInitialized = 1
 
       # ----------------------------------------------------------------
       # finally, restore active source
@@ -202,7 +165,7 @@ def CreateCoProcessor():
 
   coprocessor = CoProcessor()
   # these are the frequencies at which the coprocessor updates.
-  freqs = {'MyBHPipeline': [1, 1]}
+  freqs = {'input': [1, 1]}
   coprocessor.SetUpdateFrequencies(freqs)
   return coprocessor
 
